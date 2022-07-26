7bf76132272aff8a0ef2f702ace353892dbfb913
Safi <safikurrahman7514@gmail.com>
2022-07-26 07:26:44 +0600

    added banner and animation

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home/Home";
 import { Routes, Route } from 'react-router-dom';
 import About from './pages/About';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, [])
   return (
     <>
       <Navbar>
--- src/pages/Home/Landing.js
+++ src/pages/Home/Landing.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 const Landing = () => {
     return (
-        <div class="hero min-h-screen bg-accent">
-            <div class="hero-content flex-col">
+        <div class="hero min-h-[60vh] bg-accent">
+            <div class="hero-content max-w-6xl mx-auto uppercase flex-col lg:flex-row pt-8">
                 <div>
-                    <h1 class="text-5xl font-bold">Box Office News!</h1>
-                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button class="btn btn-primary">Get Started</button>
+                    <h2 class="lg:text-2xl font-bold text-primary" data-aos="fade-right" data-aos-duration="1000">Shine Life</h2>
+                    <h1 className='lg:text-3xl font-bold capitalize' data-aos="fade-right" data-aos-duration="2000" data-aos-delay="500">Hoarding Home Cleaning - Clutter Cleaning and Removal</h1>
+                    <p class="py-6 capitalize">Specialty cleanup and eviction clean out service in NYC. We do it all, Clutter cleaning. From junk removal to painting. Total clean out and recovery of neglected apartment home</p>
+                    <button class="btn btn-primary" data-aos="fade-right" data-aos-duration="4000" data-aos-delay="1000">Get Started</button>
                 </div>
-                <img src="https://placeimg.com/260/400/arch" class="max-w-sm rounded-lg shadow-2xl" />
+                <img className='shrink-0' src="https://i.ibb.co/PrtKD4L/banner-bg.png" />
             </div>
         </div>
     );
